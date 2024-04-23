0189100e6313434bc3abe17bc491d81d7abd761c
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-23 11:57:50 +0900

    header

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { useGetProductsQuery } from "state/api";
+import { Box } from "@mui/material";
+import Header from "components/Header";
+
 const Products = () => {
   const { data } = useGetProductsQuery();
-  return <div>Products</div>;
+  return (
+    <Box>
+      <Header title="PRODUCTS" subtitle="See your lists of products" />
+    </Box>
+  );
 };
 
 export default Products;
