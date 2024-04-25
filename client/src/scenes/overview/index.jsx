3defebd8b1974e42adf62e57d2479a2cb6f508cb
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-25 21:22:39 +0900

    form control

--- client/src/scenes/overview/index.jsx
+++ client/src/scenes/overview/index.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { FormControl, MenuItem, InputLabel, Box, Select } from "@mui/material";
+import Header from "components/Header";
+import OverviewChart from "components/OverviewChart";
 
 const Overview = () => {
-  return <div>Overview</div>;
+  const [view, setView] = useState("units");
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header
+        title="OVERVIEW"
+        subtitle="Overview of general revenue and profit"
+      />
+      <Box height="75vh">
+        <FormControl sx={{ mt: "1rem" }}>
+          <InputLabel>View</InputLabel>
+          <Select
+            value={view}
+            label="View"
+            onChange={(e) => {
+              setView(e.target.value);
+            }}
+          >
+            <MenuItem value="sales">sales</MenuItem>
+            <MenuItem value="units">Units</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </Box>
+  );
 };
 
 export default Overview;
